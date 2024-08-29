7b0a774a053087229a9a16b684e71cd0647eb6b0
Soufyane ELMARDI <soufyane.el-mardi@sogeti.com>
2024-08-29 23:23:06 +0200

    first commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class Main {
+
+    private int denominator;
+    public void setDenominator(int denominator) throws Exception{
+        if (denominator == 0)
+            throw new Exception("denominator cannot be 0");
+        this.denominator = denominator;
+    }
+
+    public static void main(String[] args) throws Exception {
+        Main main = new Main();
+        main.setDenominator(0);
+        System.out.println("Denominator set to: " + main.denominator);
+
+    }
+}
